a4d505f884d31a8a8cc94d120e310959c8a8845f
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-09-20 16:34:09 -0400

    Ask user for score

--- reinforcement-exercise-programming-fundamentals.rb
+++ reinforcement-exercise-programming-fundamentals.rb
@@ -1,29 +1,34 @@
 def grade(percentage)
   if percentage <= 100 && percentage >= 95
-    puts "A+"
+    "A+"
   elsif percentage <= 94 && percentage >= 87
-    puts "A"
+    "A"
   elsif percentage <= 85 && percentage >= 80
-    puts "A-"
+    "A-"
   elsif percentage <= 79 && percentage >= 77
-    puts "B+"
+    "B+"
   elsif percentage <= 76 && percentage >= 73
-    puts "B"
+    "B"
   elsif percentage <= 72 && percentage >= 70
-    puts "B-"
+    "B-"
   elsif percentage <= 69 && percentage >= 67
-    puts "C+"
+    "C+"
   elsif percentage <= 66 && percentage >= 63
-    puts "C"
+    "C"
   elsif percentage <= 62 && percentage >= 60
-    puts "C-"
+    "C-"
   elsif percentage <= 59 && percentage >= 57
-    puts "D+"
+    "D+"
   elsif percentage <= 56 && percentage >= 53
-    puts "D"
+    "D"
   elsif percentage <= 52 && percentage >= 50
-    puts "D-"
+    "D-"
   else
-    puts "F"
+    "F"
   end
 end
+
+
+puts "Please enter your percentage score:"
+user_score = gets.chomp.to_i
+puts"You got a #{grade(user_score)}"
