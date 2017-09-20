4f2b0f4f318092380673eec4118f749af225eb4a
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-09-20 17:49:12 -0400

    Create hashs

--- exercises.rb
+++ exercises.rb
@@ -4,3 +4,8 @@ friends_and_family_ages = [27, 28, 34, 33, 73, 67]
 flip_a_coin = [false, false, true, false, true]
 fav_artists = ["Devin Townsend", "Gojira", "SikTh"]
 fav_colours_sym = [:green, :purple, :blue]
+
+define_words = {olfactory: "Relating to the sense of smell.", translate: "Express the sense of (words or text) in another language.", BF: "A person's boyfriend"}
+fav_films = {"The Departed" => 2006, "A.I Artificial Intelligence" => 2001, "The Big Lebowski" => 1990}
+populations = {London: 8.788, Toronto: 2.615, Paris: 12310}
+family_ages = {Theo: 34, Lauren: 28, Karl: 27}
