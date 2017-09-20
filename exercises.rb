2d994f63ff56fa0efb143365880c8d82043c387f
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-09-20 17:33:19 -0400

    Create arrays

---
+++ exercises.rb
@@ -0,0 +1,6 @@
+#Exercise 0
+fav_colours = ["green", "purple", "blue"]
+friends_and_family_ages = [27, 28, 34, 33, 73, 67]
+flip_a_coin = [false, false, true, false, true]
+fav_artists = ["Devin Townsend", "Gojira", "SikTh"]
+fav_colours_sym = [:green, :purple, :blue]
