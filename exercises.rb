a779f8582fa29af66bfd88d5b5ecf7e98a2e1411
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-09-21 17:46:49 -0400

    Create method to output grocery list

--- exercises.rb
+++ exercises.rb
@@ -162,7 +162,15 @@ puts sum_total(expenses2)
 
 #Exercise 8
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
-
+grocery_list << "rice"
 grocery_list.each do |item|
   puts "* #{item}"
 end
+
+def my_groceries (groceries)
+  groceries.each do |item|
+    puts "* #{item}"
+  end
+end
+
+print my_groceries(grocery_list)
