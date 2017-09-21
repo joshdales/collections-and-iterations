74e0b750f394eb20ca6d57c30848401f84ba4705
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-09-21 17:53:34 -0400

    Check whether bananas are needed

--- exercises.rb
+++ exercises.rb
@@ -176,3 +176,9 @@ end
 print my_groceries(grocery_list)
 
 puts grocery_list.count
+
+if grocery_list.include?("bananas")
+  puts "You need to pick up bananas"
+else
+  puts "You don't need to pick up bananas today"
+end
