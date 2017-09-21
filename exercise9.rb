4e9de5070385fcb36ee00a15595b2e053eca807c
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-09-21 18:13:48 -0400

    Create method to display list of students

--- exercise9.rb
+++ exercise9.rb
@@ -3,3 +3,11 @@ students = {
   :cohort2 => 42,
   :cohort3 => 22
 }
+
+def display_students(list_of_students)
+  list_of_students.each do |cohort, number|
+    p "#{cohort}: #{number}"
+  end
+end
+
+display_students(students)
