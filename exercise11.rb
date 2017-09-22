e6624c8ac56ab9b9839c4fe2ea346b8a03822a49
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-09-22 09:53:05 -0400

    Add punctuation to exercise 11

--- exercise11.rb
+++ exercise11.rb
@@ -1,10 +1,10 @@
 
-puts "How many pizzas do you want to order"
+puts "How many pizzas do you want to order?"
 quanity = gets.chomp.to_i
 pizza_counter =  (1..quanity).to_a
 
 pizza_counter.each do |pizza_number|
-  puts "How many toppings for pizza #{pizza_number}"
+  puts "How many toppings for pizza #{pizza_number}?"
   topping_quanity = gets.chomp.to_i
-  puts "You have ordered a pizza with #{topping_quanity} toppings"
+  puts "You have ordered a pizza with #{topping_quanity} toppings."
 end
