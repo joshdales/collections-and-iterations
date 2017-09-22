c4999fa0e2671db10d3d76d58cb75ebc4b799694
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-09-22 09:51:24 -0400

    Fix typo

--- exercise11.rb
+++ exercise11.rb
@@ -6,5 +6,5 @@ pizza_counter =  (1..quanity).to_a
 pizza_counter.each do |pizza_number|
   puts "How many toppings for pizza #{pizza_number}"
   topping_quanity = gets.chomp.to_i
-  puts " you have ordered a pizza with #{topping_quanity} toppings"
+  puts "You have ordered a pizza with #{topping_quanity} toppings"
 end
