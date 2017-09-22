dcc2240d7cbcf5a0b2ef2490ff5675bc50d92bed
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-09-22 09:50:03 -0400

    Ask how many toppings for each pizza indetifying by number

--- exercise11.rb
+++ exercise11.rb
@@ -1,13 +1,10 @@
 
-
 puts "How many pizzas do you want to order"
 quanity = gets.chomp.to_i
-pizza_counter =  [1..quanity]
+pizza_counter =  (1..quanity).to_a
 
-if pizza_counter > 0
-  pizza_counter.each do |pizza_number|
-    puts "How many toppings for pizza #{pizza_number}"
-    topping_quanity = gets.chomp.to_i
-    puts " you have ordered a pizza with #{topping_quanity} toppings"
-  end
+pizza_counter.each do |pizza_number|
+  puts "How many toppings for pizza #{pizza_number}"
+  topping_quanity = gets.chomp.to_i
+  puts " you have ordered a pizza with #{topping_quanity} toppings"
 end
